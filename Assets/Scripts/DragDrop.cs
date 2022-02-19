e710c7f2a43306d6135033febc2ec6ffaec5fa22
brandon0797 <brandon12635@gmail.com>
2022-02-18 20:58:59 -0800

    Imported Mirror Networking Package
    Added Mirror package for multiplayer networking.
    Created a PlayerManager prefab to handle client side actions. Methods moved from DrawCards to Player Manager.
    Modified DragDrop to work with in multiplayer.

--- Assets/Scripts/DragDrop.cs
+++ Assets/Scripts/DragDrop.cs
@@ -1,25 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class DragDrop : MonoBehaviour
+public class DragDrop : NetworkBehaviour
 {   
     public GameObject Canvas;
+    public PlayerManager PlayerManager;
     private bool isDragging = false;
+    private bool isDraggable = true;
+    private GameObject startParent;
+    private Vector2 startPosition;
+    private GameObject dropZone;
+    private bool isOverDropZone;
     
     void Start()
     {
         Canvas = GameObject.Find("Main Canvas");
+        if (!hasAuthority)
+        {
+            isDraggable = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOverDropZone = true;
+        dropZone = collision.gameObject;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isOverDropZone = false;
+        dropZone = null;
     }
 
     public void StartDrag()
     {
+        if (!isDraggable) return;
         isDragging = true;
+        startParent = transform.parent.gameObject;
+        startPosition = transform.position;
+        
     }
 
     public void EndDrag()
     {
+        if (!isDraggable) return;
         isDragging = false;
+        if (isOverDropZone)
+        {
+            transform.SetParent(dropZone.transform, false);
+            isDraggable = false;
+            NetworkIdentity networkIdentity = NetworkClient.connection.identity;
+            PlayerManager = networkIdentity.GetComponent<PlayerManager>();
+            PlayerManager.PlayCard(gameObject);
+        }
+        else
+        {
+            transform.position = startPosition;
+            transform.SetParent(startParent.transform, false);
+        }
     }
 
     void Update()
